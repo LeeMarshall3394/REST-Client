a902a719dd9135ebba0daad7d25338505af92c11
Lee Marshall <34558251+LeeMarshall3394@users.noreply.github.com>
2019-07-31 23:00:34 +0100

    Initial commit

---
+++ REST Client/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace REST_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(84, 45);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(256, 100);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(83, 19);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(155, 20);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(244, 19);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(96, 20);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "button1";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 175);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnGo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ REST Client/restClientClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace REST_Client
+{
+    public enum httpverb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    class restClient
+    {
+        public string endPoint { get; set; }
+        public httpverb httpMethod { get; set; }
+
+        public restClient()
+        {
+            //Creating Variables
+            endPoint = string.Empty;
+            httpMethod = httpverb.GET;
+        }
+
+        public string makeRequest()
+        {
+            //Create return string
+            string strResponseValue = string.Empty;
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+            request.Method = httpMethod.ToString();
+
+            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if(response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new ApplicationException("Error Code" + response.StatusCode.ToString());
+                }
+
+                //Process response stream
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if(responseStream != null)
+                    {
+                        using(StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
